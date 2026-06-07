656e2f17241ede6eb3d2f50444c96c7ec4110e0f
NK <98242562+Nana-kwame-junior@users.noreply.github.com>
2026-06-07 13:33:29 +0000

    Replace repository with updated local project

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 class Settings:
     project_name: str = "PTA Backend"
     version: str = "0.1.0"
-    description: str = "FastAPI backend scaffold for PTA"
+    description: str = "Parent And Teacher Association Saas"
 
 
 settings = Settings()
